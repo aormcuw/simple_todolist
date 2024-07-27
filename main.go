6df29ed3d07515bebd4bfb16a0d101a3ade0b829
mfauzanz <bukanelu@gmail.com>
2024-07-27 12:46:40 +0700

    make const and struct for base

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/thedevsaddam/renderer"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func main() {
-	fmt.Println("Hello World")
-}
+var rnd *renderer.Render
+var db *mgo.Database
+
+const (
+	hostName       string = "localhost:27017"
+	dbName         string = "demo_todo"
+	colelctionName string = "todo"
+	port           string = ":9000"
+)
+
+type (
+	todoModel struct {
+		ID        bson.ObjectId `bson:"_id,omitempty"`
+		Title     string        `bson:"title"`
+		Completed bool          `bson:"compeleted"`
+		CreatedAt time.Time     `bson:"createAt"`
+	}
+	todo struct {
+		ID        bson.ObjectId `bson:"id"`
+		Title     string        `bson:"title"`
+		Completed string        `bson:"compeleted"`
+		CreatedAt time.Time     `bson:"create_at"`
+	}
+)
